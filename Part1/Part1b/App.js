d3d0ce09d8a266cf8bcb728e7985c5dc6d7f385f
Trần Duy Minh <92656809+Mdtr3002@users.noreply.github.com>
2021-11-10 10:24:08 +0700

    Update solution

--- Part1/Part1b/App.js
+++ Part1/Part1b/App.js
@@ -15,41 +15,46 @@ const Header = (headerprop) => {
   const Content = (contentprop) => {
   return(
     <>
-    <Part part={contentprop.part1} exercises={contentprop.exercises1} />
-    <Part part={contentprop.part2} exercises={contentprop.exercises2} />
-    <Part part={contentprop.part3} exercises={contentprop.exercises3} />
+    <Part part={contentprop.parts[0].name} exercises={contentprop.parts[0].exercises} />
+    <Part part={contentprop.parts[1].name} exercises={contentprop.parts[1].exercises} />
+    <Part part={contentprop.parts[2].name} exercises={contentprop.parts[2].exercises} />
     </>
   )
   }
   
-  const Footer = (footerprop) => {
+  const Footer = (footerProp) => {
     return(
-      <>
-      <p>Number of exercises {footerprop.exercises1 + footerprop.exercises2 + footerprop.exercises3}</p>
-      </>
+     <p>Number of exercises {footerProp.parts[0].exercises + footerProp.parts[1].exercises + footerProp.parts[2].exercises}</p>
     )
   }
   const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = {
-      name: 'Fundamentals of React',
-      exercises: 10
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
     }
-    const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
-    }
+  
+  
   
     return (
       <div>
-        <Header course={course}/>
-        <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
-        <Footer  exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+        <Header course={course.name}/>
+        <Content parts={course.parts}/>
+        <Footer  parts={course.parts} />
       </div>
     )
   }
 export default App;
+
